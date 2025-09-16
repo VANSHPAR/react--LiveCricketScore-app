0523bd45e62744db9c62e94df3ad3babb25428bb
VANSHPAR <vanshpar6@gmail.com>
2025-09-16 20:32:09 +0530

    Third commit

--- src/components/ScoreCard.js
+++ src/components/ScoreCard.js
@@ -2,12 +2,19 @@ import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 
 export class ScoreCard extends Component {
-  static propTypes = {}
-
+  
   render() {
+     let {name,score,matchType,status}=this.props;
     return (
-      <div>
-        This is ScoreCard item
+      <div className="my-3">
+        <div className="card" style={{width: "18rem"}}>
+          <img src="..." className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">name</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+              <a href="/" className="btn btn-sm btn-primary">Read More</a>
+            </div>
+        </div>
       </div>
     )
   }
--- src/components/Scores.js
+++ src/components/Scores.js
@@ -3,13 +3,75 @@ import React, { Component } from 'react'
 import ScoreCard from './ScoreCard'
 
 export class Scores extends Component {
-  static propTypes = {}
+  articles= [
+    {
+      "id": "2d448290-d882-4e67-9a4a-f62dfea9a51a",
+      "name": "Western Australia vs New South Wales, Final",
+      "status": "Western Australia won by 18 runs",
+      "matchType": "odi",
+      "venue": "Junction Oval, Melbourne",
+      "date": "2022-03-10",
+      "dateTimeGMT": "2022-03-10T23:30:00",
+      "teams": [
+        "Western Australia",
+        "New South Wales"
+      ],
+      "score": [
+        {
+          "r": 207,
+          "w": 10,
+          "o": 46.3,
+          "inning": "Western Australia Inning 1"
+        },
+        {
+          "r": 225,
+          "w": 9,
+          "o": 50,
+          "inning": "New South Wales Inning 1"
+        }
+      ],
+      "series_id": "ff5aa3f3-7164-4766-be90-3b64783257a0",
+      "fantasyEnabled": false
+    }
+  ]
+  constructor(){
+    super();
+    console.log("constructor from scores")
+    this.state={
+
+    }
+  }
 
   render() {
+   
     return (
-      <div>
-        This is score component
-        <ScoreCard/>
+      <div className="container my-3 ">
+        <h2>Top Matches</h2>
+        <div className="row">
+          <div className="col-md-3">
+            <ScoreCard />
+          </div>
+          <div className="col-md-3">
+            <ScoreCard />
+          </div>
+          <div className="col-md-3">
+            <ScoreCard />
+          </div>
+          <div className="col-md-3">
+            <ScoreCard />
+          </div>
+          <div className="col-md-3">
+            <ScoreCard />
+          </div>
+          <div className="col-md-3">
+            <ScoreCard />
+          </div>
+
+
+        </div>
+
+
+
 
       </div>
     )
